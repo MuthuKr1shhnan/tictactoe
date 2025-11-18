e9f3d38574b0418bf3376bea61508687a393f9ab
MuthuKr1shhnan <2000muthukrishnan@gmail.com>
2025-11-18 18:15:30 +0530

    done task

---
+++ tictactoe.js
@@ -0,0 +1,185 @@
+import readline from "readline";
+
+//0,1,2,3,4,5,6,7,8
+let positions = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+let winningPoints = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+let picker = [player, computer];
+let marked = new Array(positions.length).fill(false);
+
+// ================= TOSS
+let tose = () => {
+  let chooser = Math.floor(Math.random() * 2);
+  console.log("\nToss Winner →", chooser === 0 ? "PLAYER" : "COMPUTER");
+  picker[chooser]();
+  return;
+};
+
+// ================== READLINE SETUP
+let input = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// ================== ASK PLAYER
+function askPlayer() {
+  input.question("Enter your position (1-9)= ", (num) => {
+    let position = Number(num) - 1;
+
+    if (isNaN(position) || position < 0 || position > 8) {
+      console.log("Invalid input! Try again.");
+      return askPlayer();
+    }
+
+    if (marked[position] !== false) {
+      console.log("Position already taken!");
+      return askPlayer();
+    }
+
+    player(position, "X");
+  });
+}
+
+// ================== PRINT BOARD
+function printPositions() {
+  console.log(
+    `
+   ${positions[0]} | ${positions[1]} | ${positions[2]}
+  -----------
+   ${positions[3]} | ${positions[4]} | ${positions[5]}
+  -----------
+   ${positions[6]} | ${positions[7]} | ${positions[8]}
+  `
+  );
+}
+
+// ================= PLAYER
+function player(index, mark) {
+  if (mark === "X") {
+    positions[index] = "X";
+    marked[index] = true;
+  } else {
+    askPlayer();
+    return;
+  }
+
+  printPositions();
+
+  let first = "";
+  let second = "";
+  let third = "";
+
+  for (let idx in winningPoints) {
+    first = "";
+    second = "";
+    third = "";
+
+    for (let pos of winningPoints[idx]) {
+      if (first === "") {
+        first = pos;
+      } else if (second === "") {
+        second = pos;
+      } else if (third === "") {
+        third = pos;
+      }
+    }
+
+    if (
+      positions[first] === "X" &&
+      positions[second] === "X" &&
+      positions[third] === "X"
+    ) {
+      console.log("Player Won The Game!!!");
+      input.close();
+      return;
+    }
+  }
+
+  // DRAW CHECK
+  if (!marked.includes(false)) {
+    console.log("Match Draw!");
+    input.close();
+    return;
+  }
+
+  nextTurn(0); //
+}
+
+// ================= Machine
+function computer() {
+  console.log("\nMachine's Turn...");
+
+  let emptyPlace = [];
+  for (let i = 0; i < positions.length; i++) {
+    if (marked[i] === false) emptyPlace.push(i);
+  }
+
+  let randomPick = emptyPlace[Math.floor(Math.random() * emptyPlace.length)];
+  positions[randomPick] = "O";
+  marked[randomPick] = true;
+
+  printPositions();
+
+  // WIN CHECK (using empty string "")
+  let first = "";
+  let second = "";
+  let third = "";
+
+  for (let idx in winningPoints) {
+    first = "";
+    second = "";
+    third = "";
+
+    for (let pos of winningPoints[idx]) {
+      if (first === "") {
+        first = pos;
+      } else if (second === "") {
+        second = pos;
+      } else if (third === "") {
+        third = pos;
+      }
+    }
+
+    if (
+      positions[first] === "O" &&
+      positions[second] === "O" &&
+      positions[third] === "O"
+    ) {
+      console.log("Machine Won The Game!!!");
+      input.close();
+      return;
+    }
+  }
+
+  // DRAW CHECK
+  if (!marked.includes(false)) {
+    console.log("Match Draw!");
+    input.close();
+    return;
+  }
+
+  nextTurn(1); // next: player
+}
+
+// ================= switching players
+function nextTurn(who) {
+  if (who === 0) {
+    computer();
+    return;
+  } else {
+    askPlayer();
+    return;
+  }
+}
+
+// START GAME
+tose();
