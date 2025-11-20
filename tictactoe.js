bb5522ffd88ac2a32f056b827906717c2c7a6631
MuthuKr1shhnan <2000muthukrishnan@gmail.com>
2025-11-20 09:15:14 +0530

    made as dynamic input

--- tictactoe.js
+++ tictactoe.js
@@ -1,26 +1,122 @@
 import readline from "readline";
-
+// ================== READLINE SETUP
+let input = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 //0,1,2,3,4,5,6,7,8
-let positions = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+let positions = [];
+let marked = [];
+let boardSize = 0;
 
-let winningPoints = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-
-let marked = new Array(positions.length).fill(false);
 let playerLetter = "X";
 let computerLetter = "O";
+// START GAME
+askBoardSize();
+//=================== ASK Board size
+function askBoardSize() {
+  input.question("Enter the board size (e.g., 3 for a 3×3 board)= ", (num) => {
+    if (!isNaN(num)) {
+      for (let i = 0; i < num * num; i++) {
+        positions.push(" ");
+      }
+      for (let i = 0; i < num * num; i++) {
+        marked.push(false);
+      }
+      console.log(positions);
+      boardSize = num * num;
+      winningPointsProvider();
+      console.log(winningPoints);
+      tose();
+      return;
+    } else {
+      console.log(`Choose only numbers not letters!!!`);
+      askBoardSize();
+    }
+  });
 
-// ================= TOSS
-let tose = () => {
-  let chooser = Math.floor(Math.random() * 2);
+  return;
+}
+//Winning point desider
+
+let winningPoints = [];
+function winningPointsProvider() {
+  //row check
+  let countFrRow = Math.sqrt(boardSize);
+  let indexFrRow = 0;
+  while (countFrRow) {
+    let row = [];
+    let subCount = Math.sqrt(boardSize);
+    while (subCount) {
+      row.push(indexFrRow);
+      indexFrRow++;
+      subCount--;
+    }
+    winningPoints.push(row);
+    countFrRow--;
+  }
+  //col check
+  let countFrCol = Math.sqrt(boardSize);
+  let rowIndex = 0;
+  let indexFrCol = 0;
+  while (countFrCol) {
+    let col = [];
+    let subCount = Math.sqrt(boardSize);
+    indexFrCol = rowIndex;
+    while (subCount) {
+      col.push(indexFrCol);
+      indexFrCol += Math.sqrt(boardSize);
+      subCount--;
+    }
+    indexFrCol = 0;
+    winningPoints.push(col);
+    rowIndex++;
+    countFrCol--;
+  }
+
+  //diagonal one check from left corner
+  let countFrDiag1 = 1;
+
+  let indexStartPointDiag1 = 0;
+  let indexFrDiag1 = 0;
+  while (countFrDiag1) {
+    let diag1 = [];
+    let subCount = Math.sqrt(boardSize);
+    indexFrDiag1 = indexStartPointDiag1;
+    while (subCount) {
+      diag1.push(indexFrDiag1);
+      indexFrDiag1 += Math.sqrt(boardSize) + 1;
+      subCount--;
+    }
+
+    winningPoints.push(diag1);
+
+    countFrDiag1--;
+  }
+  //diagonal two check from right corner
+  let countFrDiag2 = 1;
+
+  let indexStartPointDiag2 = Math.sqrt(boardSize)-1;
+  let indexFrDiag2 = 0;
+  while (countFrDiag2) {
+    let diag2 = [];
+    let subCount = Math.sqrt(boardSize);
+    indexFrDiag2 = indexStartPointDiag2;
+    while (subCount) {
+      diag2.push(indexFrDiag2);
+      indexFrDiag2 += Math.sqrt(boardSize) - 1;
+      subCount--;
+    }
+
+    winningPoints.push(diag2);
+
+    countFrDiag2--;
+  }
+}
+
+// ============= TOSS
+function tose() {
+  let chooser = 0;
   console.log("\nToss Winner →", chooser === 0 ? "PLAYER" : "COMPUTER");
   if (chooser === 0) {
     selectLetter();
@@ -28,13 +124,7 @@ let tose = () => {
     computer();
   }
   return;
-};
-
-// ================== READLINE SETUP
-let input = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+}
 
 // ================== ASK PLAYER
 function askPlayer() {
@@ -55,7 +145,7 @@ function askPlayer() {
 }
 function selectLetter() {
   input.question("Choose your Letter = ", (letter) => {
-    //Trim spaces and maintains letter in uppercase for consistancy 
+    //Trim spaces and maintains letter in uppercase for consistancy
     letter = letter.trim().toUpperCase();
 
     if ((letter !== "X" && letter !== "O") || !isNaN(letter)) {
@@ -64,7 +154,7 @@ function selectLetter() {
       return;
     } else {
       playerLetter = letter;
-
+      letter === "X" ? (computerLetter = "O") : (computerLetter = "X");
       askPlayer();
       return;
     }
@@ -72,19 +162,39 @@ function selectLetter() {
 }
 // ================== PRINT BOARD
 function printPositions() {
-  console.log(
-    `
-   ${positions[0]} | ${positions[1]} | ${positions[2]}
-  -----------
-   ${positions[3]} | ${positions[4]} | ${positions[5]}
-  -----------
-   ${positions[6]} | ${positions[7]} | ${positions[8]}
-  `
-  );
+  let count = Math.sqrt(boardSize);
+  let index = 0;
+  while (count) {
+    let subCount = Math.sqrt(boardSize);
+    while (subCount) {
+      process.stdout.write(positions[index]);
+      if (subCount !== 1) {
+        process.stdout.write(" | ");
+      }
+
+      index++;
+      subCount--;
+    }
+    console.log();
+    if (count !== 1) {
+      for (
+        let i = 0;
+        i < Math.sqrt(boardSize) + (Math.sqrt(boardSize) - 1);
+        i++
+      ) {
+        process.stdout.write("- ");
+      }
+    }
+
+    console.log();
+    count--;
+  }
+
+  return;
 }
 
 // ================= PLAYER
-function player(index, mark) {
+function player(index, mark, n) {
   if (mark === "X" || mark === "O") {
     console.log(mark);
     positions[index] = mark;
@@ -93,33 +203,22 @@ function player(index, mark) {
 
   printPositions();
 
-  let first = "";
-  let second = "";
-  let third = "";
-
-  for (let idx in winningPoints) {
-    first = "";
-    second = "";
-    third = "";
-
-    for (let pos of winningPoints[idx]) {
-      if (first === "") {
-        first = pos;
-      } else if (second === "") {
-        second = pos;
-      } else if (third === "") {
-        third = pos;
+  //Condition for winning
+  for (let i = 0; i < winningPoints.length; i++) {
+    let count = 0;
+    let tem = winningPoints[i];
+    for (let j = 0; j < winningPoints.length; j++) {
+      if (positions[tem[j]] === playerLetter) {
+        console.log(tem);
+        count++;
       }
     }
-
-    if (
-      positions[first] === playerLetter &&
-      positions[second] === playerLetter &&
-      positions[third] === playerLetter
-    ) {
-      console.log("Player Won The Game!!!");
+    if (count === Math.sqrt(boardSize)) {
+      console.log("X is winner");
       input.close();
       return;
+    } else {
+      count = 0;
     }
   }
 
@@ -149,33 +248,22 @@ function computer() {
   printPositions();
 
   // WIN CHECK (using empty string "")
-  let first = "";
-  let second = "";
-  let third = "";
+  for (let i = 0; i < winningPoints.length; i++) {
+    let count = 0;
+    let tem = winningPoints[i];
 
-  for (let idx in winningPoints) {
-    first = "";
-    second = "";
-    third = "";
-
-    for (let pos of winningPoints[idx]) {
-      if (first === "") {
-        first = pos;
-      } else if (second === "") {
-        second = pos;
-      } else if (third === "") {
-        third = pos;
+    for (let j = 0; j < winningPoints.length; j++) {
+      if (positions[tem[j]] === computerLetter) {
+        console.log(tem);
+        count++;
       }
     }
-
-    if (
-      positions[first] === computerLetter &&
-      positions[second] === computerLetter &&
-      positions[third] === computerLetter
-    ) {
-      console.log("Machine Won The Game!!!");
+    if (count === Math.sqrt(boardSize)) {
+      console.log("Machine is winner");
       input.close();
       return;
+    } else {
+      count = 0;
     }
   }
 
@@ -199,6 +287,3 @@ function nextTurn(who) {
     return;
   }
 }
-
-// START GAME
-tose();
